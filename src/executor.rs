f8bcae453067d98b40549e5c9e3543880d4dd96b
Evan Putnam <emput1998@gmail.com>
2025-12-16 19:51:58 -0500

    Making multiplatform

--- src/executor.rs
+++ src/executor.rs
@@ -1,16 +1,11 @@
 use chrono::Local;
 use reqwest::blocking::get;
-use std::ffi::OsStr;
 use std::fs::File;
 use std::io::Write;
-use std::os::raw::c_void;
-use std::os::windows::ffi::OsStrExt;
 use std::path::Path;
 use std::thread;
 use std::time::Duration;
-use windows::Win32::UI::WindowsAndMessaging::{
-    SPI_SETDESKWALLPAPER, SYSTEM_PARAMETERS_INFO_UPDATE_FLAGS, SystemParametersInfoW,
-};
+use wallpaper;
 
 pub fn download_loop(url: &str, img_path: &Path, initial_wait_minutes: u64, refresh_minutes: u64) {
     println!(
@@ -51,23 +46,5 @@ fn download_image(url: &str, path: &Path) -> Result<(), Box<dyn std::error::Erro
 
 fn set_wallpaper(path: &Path) -> Result<(), Box<dyn std::error::Error>> {
     let path_str = path.to_str().ok_or("Wallpaper path is not valid UTF-8")?;
-    let wide: Vec<u16> = OsStr::new(path_str)
-        .encode_wide()
-        .chain(std::iter::once(0))
-        .collect();
-
-    let result = unsafe {
-        SystemParametersInfoW(
-            SPI_SETDESKWALLPAPER,
-            0,
-            Some(wide.as_ptr() as *mut c_void),
-            SYSTEM_PARAMETERS_INFO_UPDATE_FLAGS(0),
-        )
-    };
-
-    if result.is_ok() {
-        Ok(())
-    } else {
-        Err("SystemParametersInfoW failed".into())
-    }
+    wallpaper::set_from_path(path_str)
 }
